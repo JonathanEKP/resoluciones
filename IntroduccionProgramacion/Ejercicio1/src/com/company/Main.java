145add8783d2a683aba463cc8658f09cbc8baf78
Jonathan Larios <jonathanvaldez2001@gmail.com>
2022-03-30 22:35:51 -0600

    Ejercicio1 subido

---
+++ IntroduccionProgramacion/Ejercicio1/src/com/company/Main.java
@@ -0,0 +1,30 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+       //Parte uno
+        var resultado =  suma(10, 15,20);
+        System.out.print("Resultado de la suma: ");
+        System.out.println(resultado);
+        //Parte dos
+        Coche miCoche = new Coche();
+        miCoche.AgregarPuertas();
+        System.out.print("Numero de puertas: ");
+        System.out.println(miCoche.puertas);
+    }
+
+    public static int suma (int a, int b, int c){
+        return a+b+c;
+    }
+
+
+}
+
+class Coche{
+    int puertas = 2;
+
+    public void AgregarPuertas(){
+        this.puertas++;
+    }
+}
