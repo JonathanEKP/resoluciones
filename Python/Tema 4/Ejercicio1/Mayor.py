8710b84499d5a5d6d38d1fcf753e02b3508dc57e
Jonathan Larios <jonathanvaldez2001@gmail.com>
2022-05-20 22:27:38 -0600

    Ejercicio 1 de tema 4 resuelto

---
+++ Python/Tema 4/Ejercicio1/Mayor.py
@@ -0,0 +1,6 @@
+edad = int(input("¿Cual es tu edad?"))
+
+if (edad>18):
+    print("El usuario es mayor de edad")
+else :
+    print("El usuario es menor de edad")
---
+++ Python/Tema 4/Ejercicio2/impares.py
@@ -0,0 +1,8 @@
+num1 = 2
+num2 = 8
+
+while num1 <= num2 :
+    if num1 % 2 >0:
+        print(num1)
+        num1+=1
+    
